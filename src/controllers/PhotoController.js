44819f8c62040d4a0366d92fb6668687088566ad
Frank Cunha <franklinfsc42@gmail.com>
2023-03-31 13:11:56 -0300

    Adicionando arquivos

---
+++ src/controllers/PhotoController.js
@@ -0,0 +1,34 @@
+/* eslint-disable camelcase */
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+import Photo from '../models/Photo';
+
+const upload = multer(multerConfig).single('photo');
+
+class PhotoController {
+  store(req, res) {
+    return upload(req, res, async (error) => {
+      if (error) {
+        return res.status(400).json({
+          errors: [error.code],
+        });
+      }
+
+      try {
+        const { originalname, filename } = req.file;
+        const { aluno_id } = req.body;
+        console.log(originalname, filename, aluno_id);
+        const photo = await Photo.create({ originalname, filename, aluno_id });
+
+        return res.json(photo);
+      } catch (e) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+    });
+  }
+}
+
+export default new PhotoController();
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import userController from '../controllers/UserController';
+import loginRequired from '../middlewares/loginRequired';
+
+const router = new Router();
+
+// Não deveria existir
+router.get('/', loginRequired, userController.index); // Lista usuários
+// router.get('/:id', userController.show); // Lista usuário
+
+router.post('/', userController.store); // Cria usuário
+router.put('/', loginRequired, userController.update); // Altera usuário
+router.delete('/', loginRequired, userController.delete); // Deleta usuário
+
+export default router;
+
+/*
+index -> lista todos os usuários -> GET
+store/create -> cria um novo usuário -> POST
+delete -> apaga um usuário -> DELETE
+show -> mostra um usuário -> GET
+update -> atualiza um usuário -> PATCH ou PUT
+*/
